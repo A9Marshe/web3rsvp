4aa7cc99aa16281abcba877c419ada77d628fc06
Alex <alkhateeb.alex@hotmail.com>
2022-08-24 13:20:19 +0300

    -added deploy script -fixed HH config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,18 @@
 require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: process.env.STAGING_ALCHEMY_URL,
+      accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
+      gas: 2100000,
+      gasPrice: 8000000000,
+    },
+  },
 };
